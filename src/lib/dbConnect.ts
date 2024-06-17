a6523194f81288e2b8cc68f4a057233d4c2c334e
unknown <raunakmishra6300@gmail.com>
2024-06-18 01:01:20 +0530

    Added MongoDB connection logic

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number
+}// this is a type definition for the connection object which will be returned by the function when database is connected
+
+const connection: ConnectionObject = {};// creating an object of type ConnectionObject
+
+async function dbConnect():Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already connected to database");
+        return;
+    }
+    // connecting to the database
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "")
+        console.log(db)
+        connection.isConnected = db.connections[0].readyState;
+        console.log("DB Connected Successfully");
+    } catch (error) {
+        console.log("Database connection failed!: ", error);
+        process.exit(1);
+    }
+}
